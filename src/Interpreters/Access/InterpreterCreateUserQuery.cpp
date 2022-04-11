c7a337b91a150d2f6393d82f68de01bff58b3aa9
Rajkumar <rajkumar.varada@ibm.com>
2022-04-11 10:06:23 -0700

    empty spaces handled

--- src/Interpreters/Access/InterpreterCreateUserQuery.cpp
+++ src/Interpreters/Access/InterpreterCreateUserQuery.cpp
@@ -38,7 +38,6 @@ namespace
         if (query.auth_data)
         {
             user.auth_data = *query.auth_data;
-
             //User and  query IDENTIFIED WITH AUTHTYPE PLAINTEXT and NO_PASSWORD should not be allowed if allow_plaintext_and_no_password is unset.
             if ((query.auth_data->getType() == AuthenticationType::PLAINTEXT_PASSWORD  && !allow_plaintext_password) || (query.auth_data->getType() == AuthenticationType::NO_PASSWORD &&  !allow_no_password))
                 throw Exception(ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT, "User is not allowed to ALTER/CREATE USERS with type "+ toString(query.auth_data->getType())+". Please configure User with authtype"
