bfae672c6b8b7c3fe5fdf9279eff2a48bf452011
Yong Wang <yong.wang@ibm.com>
2022-05-30 12:17:18 -0700

    KQL support for CH
    Added Limit,Sort, Filter, and Operatopr

--- src/Parsers/ParserKQLTable.h
+++ src/Parsers/ParserKQLTable.h
@@ -9,7 +9,6 @@ namespace DB
 class ParserKQLTable : public ParserKQLBase
 {
 protected:
-    
     const char * getName() const override { return "KQL Table"; }
     bool parseImpl(Pos & pos, ASTPtr & node, Expected & expected) override;
     bool parsePrepare(Pos &pos) override;
