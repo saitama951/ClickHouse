903ad0a8b3f3c6623847c9d8cad2666e21bf6dbd
Yong Wang <yong.wang@ibm.com>
2022-06-23 14:26:37 -0700

    Kusto-phase2: add kusto_auto dialect

--- src/Client/ClientBase.cpp
+++ src/Client/ClientBase.cpp
@@ -325,6 +325,8 @@ ASTPtr ClientBase::parseQuery(const char *& pos, const char * end, bool allow_mu
     const auto & settings = global_context->getSettingsRef();
     size_t max_length = 0;
 
+    auto begin = pos;
+
     if (!allow_multi_statements)
         max_length = settings.max_query_size;
 
@@ -353,8 +355,14 @@ ASTPtr ClientBase::parseQuery(const char *& pos, const char * end, bool allow_mu
 
         if (!res)
         {
-            std::cerr << std::endl << message << std::endl << std::endl;
-            return nullptr;
+            if (sql_dialect != "kusto")
+                res = tryParseQuery(kql_parser, begin, end, message, true, "", allow_multi_statements, max_length, settings.max_parser_depth);
+
+            if (!res)
+            {
+                std::cerr << std::endl << message << std::endl << std::endl;
+                return nullptr;
+            }
         }
     }
     else
