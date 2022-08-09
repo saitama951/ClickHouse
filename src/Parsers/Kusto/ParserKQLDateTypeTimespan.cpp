5228fd0cd820537fe726435846c1242ae03ef7d4
kashwy <yong.wang@ibm.com>
2022-08-09 06:11:39 -0700

    Kusto-phase2: Add kusto data types
    (cherry picked from commit 1048d31e030f61a23dedd1530dde8c2c1756de18)

--- src/Parsers/Kusto/ParserKQLDateTypeTimespan.cpp
+++ src/Parsers/Kusto/ParserKQLDateTypeTimespan.cpp
@@ -2,25 +2,18 @@
 #include <Parsers/ExpressionListParsers.h>
 #include <Parsers/Kusto/ParserKQLQuery.h>
 #include <Parsers/Kusto/ParserKQLDateTypeTimespan.h>
-#include <Common/StringUtils/StringUtils.h>
 #include <cstdlib>
 #include <unordered_map>
 #include <format>
-#include <math.h> 
 
 namespace DB
 {
 
 bool ParserKQLDateTypeTimespan :: parseImpl(Pos & pos,  [[maybe_unused]] ASTPtr & node, Expected & expected)
 {
-    String token;
+    const String token(pos->begin,pos->end);
     const char * current_word = pos->begin;
     expected.add(pos, current_word);
-
-    if (pos->type == TokenType::QuotedIdentifier || pos->type == TokenType::StringLiteral )
-        token = String(pos->begin + 1, pos->end -1);
-    else
-        token = String(pos->begin, pos->end);
 
     if (!parseConstKQLTimespan(token))
         return false;
@@ -91,8 +84,6 @@ bool ParserKQLDateTypeTimespan :: parseConstKQLTimespan(const String & text)
         {"ticks", KQLTimespanUint::tick}
     };
 
-    uint16_t days = 0, hours = 0, minutes = 0, seconds = 0 , sec_scale_len = 0;
-    double nanoseconds = 00.00;
 
     const char * ptr = text.c_str();
 
@@ -108,77 +99,21 @@ bool ParserKQLDateTypeTimespan :: parseConstKQLTimespan(const String & text)
     if (number_len <= 0)
         return false;
 
-    days = std::stoi(String(ptr, ptr + number_len));
-
     if (*(ptr + number_len) == '.')
     {
         auto fractionLen = scanDigit(ptr + number_len + 1);
         if (fractionLen >= 0)
         {
-            hours = std::stoi(String(ptr + number_len + 1, ptr + number_len + 1 + fractionLen));
             number_len += fractionLen + 1;
         }
-        else
-        {
-            hours = days;
-            days = 0;
-        }
     }
 
-    if (hours > 23)
+    String timespan_suffix(ptr + number_len, ptr+text.size());
+    if (TimespanSuffixes.find(timespan_suffix) == TimespanSuffixes.end())
         return false;
 
-    if (*(ptr + number_len) != ':')
-    {
-        String timespan_suffix(ptr + number_len, ptr + text.size());
-
-        trim(timespan_suffix);
-        if (TimespanSuffixes.find(timespan_suffix) == TimespanSuffixes.end())
-            return false;
-
-        time_span = std::stod(String(ptr, ptr + number_len));
-        time_span_unit = TimespanSuffixes[timespan_suffix] ;
-
-        return true;
-    }
-
-    auto min_len = scanDigit(ptr + number_len + 1);
-    if (min_len < 0)
-        return false;
-
-    minutes = std::stoi(String(ptr + number_len + 1, ptr + number_len + 1 + min_len));
-    if (minutes > 59)
-        return false;
-
-    number_len += min_len + 1;
-    if (*(ptr + number_len) == ':')
-    {
-        auto sec_len = scanDigit(ptr + number_len + 1);
-        if (sec_len > 0)
-        {
-            seconds = std::stoi(String(ptr + number_len + 1, ptr + number_len + 1 + sec_len));
-            if (seconds > 59)
-                return false;
-
-            number_len += sec_len + 1;
-            if (*(ptr + number_len) == '.')
-            {
-                sec_scale_len = scanDigit(ptr + number_len + 1);
-                if (sec_scale_len > 0)
-                {
-                    nanoseconds = std::stoi(String(ptr + number_len + 1, ptr + number_len + 1 + sec_scale_len));
-
-                    if (nanoseconds > 1000000000)
-                        return false;
-                }
-            }
-        }
-    }
-    auto exponent = 9 - sec_scale_len; // max supported length of fraction of seconds is 9 
-    nanoseconds = nanoseconds * pow(10, exponent );
-
-    time_span = days * 86400 + hours * 3600 + minutes * 60 + seconds + (nanoseconds /1000000000 );
-    time_span_unit = KQLTimespanUint::second;
+    time_span = std::stod(String(ptr, ptr + number_len));
+    time_span_unit =TimespanSuffixes[timespan_suffix] ;
 
     return true;
 }
