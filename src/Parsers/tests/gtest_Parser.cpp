902436abb83d1365119516ea07f118cfba59a2bc
ltrk2 <107155950+ltrk2@users.noreply.github.com>
2022-08-10 07:46:29 -0700

    Implement KQL binary functions
    (cherry picked from commit ef81bfba2719c9c2156c13f659f27127596c06bb)

--- src/Parsers/tests/gtest_Parser.cpp
+++ src/Parsers/tests/gtest_Parser.cpp
@@ -16,7 +16,6 @@
 #include <Parsers/Kusto/ParserKQLQuery.h>
 #include <string_view>
 #include <regex>
-#include <gtest/gtest.h>
 
 namespace
 {
@@ -32,6 +31,48 @@ std::ostream & operator<<(std::ostream & ostr, const std::shared_ptr<IParser> pa
 std::ostream & operator<<(std::ostream & ostr, const ParserTestCase & test_case)
 {
     return ostr << "ParserTestCase input: " << test_case.input_text;
+}
+
+TEST_P(ParserTest, parseQuery)
+{
+    const auto & parser = std::get<0>(GetParam());
+    const auto & [input_text, expected_ast] = std::get<1>(GetParam());
+
+    ASSERT_NE(nullptr, parser);
+
+    if (expected_ast)
+    {
+        if (std::string(expected_ast).starts_with("throws"))
+        {
+            EXPECT_THROW(parseQuery(*parser, input_text.begin(), input_text.end(), 0, 0), DB::Exception);
+        }
+        else
+        {
+            ASTPtr ast;
+            ASSERT_NO_THROW(ast = parseQuery(*parser, input_text.begin(), input_text.end(), 0, 0));
+            if (std::string("CREATE USER or ALTER USER query") != parser->getName()
+                    && std::string("ATTACH access entity query") != parser->getName())
+            {
+                EXPECT_EQ(expected_ast, serializeAST(*ast->clone(), false));
+            }
+            else
+            {
+                if (input_text.starts_with("ATTACH"))
+                {
+                    auto salt = (dynamic_cast<const ASTCreateUserQuery *>(ast.get())->auth_data)->getSalt();
+                    EXPECT_TRUE(std::regex_match(salt, std::regex(expected_ast)));
+                }
+                else
+                {
+                    EXPECT_TRUE(std::regex_match(serializeAST(*ast->clone(), false), std::regex(expected_ast)));
+                }
+            }
+        }
+    }
+    else
+    {
+        ASSERT_THROW(parseQuery(*parser, input_text.begin(), input_text.end(), 0, 0), DB::Exception);
+    }
 }
 
 INSTANTIATE_TEST_SUITE_P(ParserOptimizeQuery, ParserTest,
