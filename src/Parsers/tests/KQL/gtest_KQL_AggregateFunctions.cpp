aaefd2f909c00ef1fe9b3c65bb2de5bc08ae0e0d
root <mallik.hassan@ibm.com>
2022-08-17 18:42:46 -0700

    Aggregate functions added
    (cherry picked from commit 967bbdfbe57560eedc96914940e1a090ef652ad0)

--- src/Parsers/tests/KQL/gtest_KQL_AggregateFunctions.cpp
+++ src/Parsers/tests/KQL/gtest_KQL_AggregateFunctions.cpp
@@ -1,7 +1,71 @@
 #include <Parsers/tests/gtest_common.h>
+#include <IO/WriteBufferFromOStream.h>
+#include <Interpreters/applyTableOverride.h>
+#include <Parsers/ASTCreateQuery.h>
+#include <Parsers/ASTFunction.h>
+#include <Parsers/ASTIdentifier.h>
+#include <Parsers/Access/ASTCreateUserQuery.h>
+#include <Parsers/Access/ParserCreateUserQuery.h>
+#include <Parsers/ParserAlterQuery.h>
+#include <Parsers/ParserCreateQuery.h>
+#include <Parsers/ParserOptimizeQuery.h>
+#include <Parsers/ParserQueryWithOutput.h>
+#include <Parsers/ParserAttachAccessEntity.h>
+#include <Parsers/formatAST.h>
+#include <Parsers/parseQuery.h>
 #include <Parsers/Kusto/ParserKQLQuery.h>
+#include <string_view>
+#include <regex>
+#include <gtest/gtest.h>
 
-INSTANTIATE_TEST_SUITE_P(ParserKQLQuery, ParserTest,
+namespace
+{
+using namespace DB;
+using namespace std::literals;
+}
+class ParserAggregateFuncTest : public ::testing::TestWithParam<std::tuple<std::shared_ptr<DB::IParser>, ParserTestCase>>
+{};
+
+TEST_P(ParserAggregateFuncTest, ParseQuery)
+{  const auto & parser = std::get<0>(GetParam());
+    const auto & [input_text, expected_ast] = std::get<1>(GetParam());
+    ASSERT_NE(nullptr, parser);
+    if (expected_ast)
+    {
+        if (std::string(expected_ast).starts_with("throws"))
+        {
+            EXPECT_THROW(parseQuery(*parser, input_text.begin(), input_text.end(), 0, 0), DB::Exception);
+        }
+        else
+        {
+            ASTPtr ast;
+            ASSERT_NO_THROW(ast = parseQuery(*parser, input_text.begin(), input_text.end(), 0, 0));
+            if (std::string("CREATE USER or ALTER USER query") != parser->getName()
+                    && std::string("ATTACH access entity query") != parser->getName())
+            {
+                EXPECT_EQ(expected_ast, serializeAST(*ast->clone(), false));
+            }
+            else
+            {
+                if (input_text.starts_with("ATTACH"))
+                {
+                    auto salt = (dynamic_cast<const ASTCreateUserQuery *>(ast.get())->auth_data)->getSalt();
+                    EXPECT_TRUE(std::regex_match(salt, std::regex(expected_ast)));
+                }
+                else
+                {
+                    EXPECT_TRUE(std::regex_match(serializeAST(*ast->clone(), false), std::regex(expected_ast)));
+                }
+            }
+        }
+    }
+    else
+    {
+        ASSERT_THROW(parseQuery(*parser, input_text.begin(), input_text.end(), 0, 0), DB::Exception);
+    }
+}
+
+INSTANTIATE_TEST_SUITE_P(ParserKQLQuery, ParserAggregateFuncTest,
     ::testing::Combine(
         ::testing::Values(std::make_shared<DB::ParserKQLQuery>()),
         ::testing::ValuesIn(std::initializer_list<ParserTestCase>{
@@ -45,13 +109,5 @@ INSTANTIATE_TEST_SUITE_P(ParserKQLQuery, ParserTest,
         {
             "DataTable| summarize t = percentilesw_array(Bucket, Frequency, dynamic([10, 50, 30]))",
             "SELECT quantilesExactWeighted(10 / 100, 50 / 100, 30 / 100)(Bucket, Frequency) AS t\nFROM DataTable"
-        },
-        {
-            "Customers | summarize t = percentile(Age, 50) by FirstName",
-            "SELECT\n    FirstName,\n    quantile(50 / 100)(Age) AS t\nFROM Customers\nGROUP BY FirstName"
-        },
-        {
-            "DataTable | summarize t = percentilew(Bucket, Frequency, 50)",
-            "SELECT quantileExactWeighted(50 / 100)(Bucket, Frequency) AS t\nFROM DataTable"
         }
 })));
