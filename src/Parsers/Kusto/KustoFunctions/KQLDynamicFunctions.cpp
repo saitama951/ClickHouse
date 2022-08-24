7517c5b633a615ed3a326eaa44c788bfc921fde9
root <mallik.hassan@ibm.com>
2022-08-24 07:40:16 -0700

    retry resolve merge conflict
    (cherry picked from commit 802b92f6829030d2f9a42530ef5997519c5c349d)

--- src/Parsers/Kusto/KustoFunctions/KQLDynamicFunctions.cpp
+++ src/Parsers/Kusto/KustoFunctions/KQLDynamicFunctions.cpp
@@ -14,25 +14,31 @@
 namespace DB
 {
 
-bool ArrayConcat::convertImpl(String & out,IParser::Pos & pos)
+bool ArrayConcat::convertImpl(String & out, IParser::Pos & pos)
 {
     return directMapping(out, pos, "arrayConcat");
 }
 
-bool ArrayIif::convertImpl(String & out,IParser::Pos & pos)
+bool ArrayIif::convertImpl(String & out, IParser::Pos & pos)
 {
     const auto function_name = getKQLFunctionName(pos);
     if (function_name.empty())
         return false;
 
-bool ArrayIndexOf::convertImpl(String & out,IParser::Pos & pos)
-{
-    String res = String(pos->begin,pos->end);
-    out = res;
-    return false;
+    const auto conditions = getArgument(function_name, pos);
+    const auto if_true = getArgument(function_name, pos);
+    const auto if_false = getArgument(function_name, pos);
+
+    out = std::format(
+        "arrayMap(x -> if(x.1 != 0, x.2, x.3), arrayZip({0}, arrayResize({1}, length({0}), null), arrayResize({2}, length({0}), null)))",
+        conditions,
+        if_true,
+        if_false);
+
+    return true;
 }
 
-bool ArrayLength::convertImpl(String & out,IParser::Pos & pos)
+bool ArrayIndexOf::convertImpl(String & out, IParser::Pos & pos)
 {
     const auto fn_name = getKQLFunctionName(pos);
     if (fn_name.empty())
@@ -45,70 +51,82 @@ bool ArrayLength::convertImpl(String & out,IParser::Pos & pos)
     return true;
 }
 
-bool ArrayReverse::convertImpl(String & out,IParser::Pos & pos)
+bool ArrayLength::convertImpl(String & out, IParser::Pos & pos)
 {
     return directMapping(out, pos, "length");
 }
 
-bool ArrayRotateLeft::convertImpl(String & out,IParser::Pos & pos)
+bool ArrayReverse::convertImpl(String & out, IParser::Pos & pos)
 {
     String res = String(pos->begin, pos->end);
     out = res;
     return false;
 }
 
-bool ArrayRotateRight::convertImpl(String & out,IParser::Pos & pos)
+bool ArrayRotateLeft::convertImpl(String & out, IParser::Pos & pos)
 {
     String res = String(pos->begin, pos->end);
     out = res;
     return false;
 }
 
-bool ArrayShiftLeft::convertImpl(String & out,IParser::Pos & pos)
+bool ArrayRotateRight::convertImpl(String & out, IParser::Pos & pos)
 {
     String res = String(pos->begin, pos->end);
     out = res;
     return false;
 }
 
-bool ArrayShiftRight::convertImpl(String & out,IParser::Pos & pos)
+bool ArrayShiftLeft::convertImpl(String & out, IParser::Pos & pos)
 {
     String res = String(pos->begin, pos->end);
     out = res;
     return false;
 }
 
-bool ArraySlice::convertImpl(String & out,IParser::Pos & pos)
+bool ArrayShiftRight::convertImpl(String & out, IParser::Pos & pos)
 {
     String res = String(pos->begin, pos->end);
     out = res;
     return false;
 }
 
-bool ArraySortAsc::convertImpl(String & out,IParser::Pos & pos)
+bool ArraySlice::convertImpl(String & out, IParser::Pos & pos)
 {
-    out = ArraySortHelper(out, pos, true);
-    if(out == "false")
+    const auto function_name = getKQLFunctionName(pos);
+    if (function_name.empty())
         return false;
+
+    const auto array = getArgument(function_name, pos);
+    const auto start = getArgument(function_name, pos);
+    const auto end = getArgument(function_name, pos);
+
+    out = std::format(
+        "arraySlice({0}, plus(1, if({1} >= 0, {1}, toInt64(max2(-length({0}), {1})) + length({0}))) as offset_{3}, "
+        "                plus(1, if({2} >= 0, {2}, toInt64(max2(-length({0}), {2})) + length({0}))) - offset_{3} + 1)",
+        array,
+        start,
+        end,
+        generateUniqueIdentifier());
+
     return true;
 }
 
-bool ArraySortDesc::convertImpl(String & out,IParser::Pos & pos)
-{
-    out = ArraySortHelper(out, pos, false);
-    if(out == "false")
-        return false;
-    return true;
-}
-
-bool ArraySplit::convertImpl(String & out,IParser::Pos & pos)
+bool ArraySortAsc::convertImpl(String & out, IParser::Pos & pos)
 {
     String res = String(pos->begin, pos->end);
     out = res;
     return false;
 }
 
-bool ArraySum::convertImpl(String & out,IParser::Pos & pos)
+bool ArraySortDesc::convertImpl(String & out, IParser::Pos & pos)
+{
+    String res = String(pos->begin, pos->end);
+    out = res;
+    return false;
+}
+
+bool ArraySplit::convertImpl(String & out, IParser::Pos & pos)
 {
     const auto function_name = getKQLFunctionName(pos);
     if (function_name.empty())
@@ -134,98 +152,98 @@ bool ArraySum::convertImpl(String & out, IParser::Pos & pos)
     return directMapping(out, pos, "arraySum");
 }
 
-bool BagKeys::convertImpl(String & out,IParser::Pos & pos)
+bool BagKeys::convertImpl(String & out, IParser::Pos & pos)
 {
     String res = String(pos->begin, pos->end);
     out = res;
     return false;
 }
 
-bool BagMerge::convertImpl(String & out,IParser::Pos & pos)
+bool BagMerge::convertImpl(String & out, IParser::Pos & pos)
 {
     String res = String(pos->begin, pos->end);
     out = res;
     return false;
 }
 
-bool BagRemoveKeys::convertImpl(String & out,IParser::Pos & pos)
+bool BagRemoveKeys::convertImpl(String & out, IParser::Pos & pos)
 {
     String res = String(pos->begin, pos->end);
     out = res;
     return false;
 }
 
-bool JaccardIndex::convertImpl(String & out,IParser::Pos & pos)
+bool JaccardIndex::convertImpl(String & out, IParser::Pos & pos)
 {
     String res = String(pos->begin, pos->end);
     out = res;
     return false;
 }
 
-bool Pack::convertImpl(String & out,IParser::Pos & pos)
+bool Pack::convertImpl(String & out, IParser::Pos & pos)
 {
     String res = String(pos->begin, pos->end);
     out = res;
     return false;
 }
 
-bool PackAll::convertImpl(String & out,IParser::Pos & pos)
+bool PackAll::convertImpl(String & out, IParser::Pos & pos)
 {
     String res = String(pos->begin, pos->end);
     out = res;
     return false;
 }
 
-bool PackArray::convertImpl(String & out,IParser::Pos & pos)
+bool PackArray::convertImpl(String & out, IParser::Pos & pos)
 {
     String res = String(pos->begin, pos->end);
     out = res;
     return false;
 }
 
-bool Repeat::convertImpl(String & out,IParser::Pos & pos)
+bool Repeat::convertImpl(String & out, IParser::Pos & pos)
 {
     String res = String(pos->begin, pos->end);
     out = res;
     return false;
 }
 
-bool SetDifference::convertImpl(String & out,IParser::Pos & pos)
+bool SetDifference::convertImpl(String & out, IParser::Pos & pos)
 {
     String res = String(pos->begin, pos->end);
     out = res;
     return false;
 }
 
-bool SetHasElement::convertImpl(String & out,IParser::Pos & pos)
+bool SetHasElement::convertImpl(String & out, IParser::Pos & pos)
 {
     String res = String(pos->begin, pos->end);
     out = res;
     return false;
 }
 
-bool SetIntersect::convertImpl(String & out,IParser::Pos & pos)
+bool SetIntersect::convertImpl(String & out, IParser::Pos & pos)
 {
     String res = String(pos->begin, pos->end);
     out = res;
     return false;
 }
 
-bool SetUnion::convertImpl(String & out,IParser::Pos & pos)
+bool SetUnion::convertImpl(String & out, IParser::Pos & pos)
 {
     String res = String(pos->begin, pos->end);
     out = res;
     return false;
 }
 
-bool TreePath::convertImpl(String & out,IParser::Pos & pos)
+bool TreePath::convertImpl(String & out, IParser::Pos & pos)
 {
     String res = String(pos->begin, pos->end);
     out = res;
     return false;
 }
 
-bool Zip::convertImpl(String & out,IParser::Pos & pos)
+bool Zip::convertImpl(String & out, IParser::Pos & pos)
 {
     String res = String(pos->begin, pos->end);
     out = res;
--- src/Parsers/tests/KQL/gtest_KQL_dateTimeFunctions.cpp
+++ src/Parsers/tests/KQL/gtest_KQL_dateTimeFunctions.cpp
@@ -25,7 +25,7 @@ INSTANTIATE_TEST_SUITE_P(ParserKQLQuery, ParserTest,
         },
         {
             "print startofyear(datetime(2017-01-01 10:10:17), -1)",
-            "SELECT parseDateTime64BestEffortOrNull(toStartOfYear(parseDateTime64BestEffortOrNull('2017-01-01 10:10:17', 9, 'UTC'), 'UTC'), 9, 'UTC') + toIntervalYear(-1)"
+            "SELECT toDateTime64(toStartOfYear(toDateTime64('2017-01-01 10:10:17', 9, 'UTC'), 'UTC'), 9, 'UTC') + toIntervalYear(-1)"
         },
         {
             "print monthofyear(datetime(2015-12-14))",
@@ -64,6 +64,7 @@ INSTANTIATE_TEST_SUITE_P(ParserKQLQuery, ParserTest,
             "SELECT now64(9, 'UTC')"
         },
         {
+
             "print now(1d)",
             "SELECT now64(9, 'UTC') + 86400."
         },
@@ -73,35 +74,36 @@ INSTANTIATE_TEST_SUITE_P(ParserKQLQuery, ParserTest,
         },  
         {
             "print endofday(datetime(2017-01-01 10:10:17), -1)",
-            "SELECT (toDateTime(toStartOfDay(parseDateTime64BestEffortOrNull('2017-01-01 10:10:17', 9, 'UTC')), 9, 'UTC') + toIntervalDay(-1 + 1)) - toIntervalMicrosecond(1)"
+            "SELECT (toDateTime(toStartOfDay(toDateTime64('2017-01-01 10:10:17', 9, 'UTC')), 9, 'UTC') + toIntervalDay(-1 + 1)) - toIntervalMicrosecond(1)"
         },
         {
             "print endofday(datetime(2017-01-01 10:10:17), 1)",
-            "SELECT (toDateTime(toStartOfDay(parseDateTime64BestEffortOrNull('2017-01-01 10:10:17', 9, 'UTC')), 9, 'UTC') + toIntervalDay(1 + 1)) - toIntervalMicrosecond(1)"
+            "SELECT (toDateTime(toStartOfDay(toDateTime64('2017-01-01 10:10:17', 9, 'UTC')), 9, 'UTC') + toIntervalDay(1 + 1)) - toIntervalMicrosecond(1)"
+
         },
         {
             "print endofmonth(datetime(2017-01-01 10:10:17), -1)",
-            "SELECT (toDateTime(toStartOfDay(parseDateTime64BestEffortOrNull('2017-01-01 10:10:17', 9, 'UTC')), 9, 'UTC') + toIntervalMonth(-1 + 1)) - toIntervalMicrosecond(1)"
+            "SELECT (toDateTime(toStartOfDay(toDateTime64('2017-01-01 10:10:17', 9, 'UTC')), 9, 'UTC') + toIntervalMonth(-1 + 1)) - toIntervalMicrosecond(1)"
         },
         {
             "print endofmonth(datetime(2017-01-01 10:10:17), 1)",
-            "SELECT (toDateTime(toStartOfDay(parseDateTime64BestEffortOrNull('2017-01-01 10:10:17', 9, 'UTC')), 9, 'UTC') + toIntervalMonth(1 + 1)) - toIntervalMicrosecond(1)"
+            "SELECT (toDateTime(toStartOfDay(toDateTime64('2017-01-01 10:10:17', 9, 'UTC')), 9, 'UTC') + toIntervalMonth(1 + 1)) - toIntervalMicrosecond(1)"
         },
         {
             "print endofweek(datetime(2017-01-01 10:10:17), -1)",
-            "SELECT (toDateTime(toStartOfDay(parseDateTime64BestEffortOrNull('2017-01-01 10:10:17', 9, 'UTC')), 9, 'UTC') + toIntervalWeek(-1 + 1)) - toIntervalMicrosecond(1)"
+            "SELECT (toDateTime(toStartOfDay(toDateTime64('2017-01-01 10:10:17', 9, 'UTC')), 9, 'UTC') + toIntervalWeek(-1 + 1)) - toIntervalMicrosecond(1)"
         },
         {
             "print endofweek(datetime(2017-01-01 10:10:17), 1)",
-            "SELECT (toDateTime(toStartOfDay(parseDateTime64BestEffortOrNull('2017-01-01 10:10:17', 9, 'UTC')), 9, 'UTC') + toIntervalWeek(1 + 1)) - toIntervalMicrosecond(1)"
+            "SELECT (toDateTime(toStartOfDay(toDateTime64('2017-01-01 10:10:17', 9, 'UTC')), 9, 'UTC') + toIntervalWeek(1 + 1)) - toIntervalMicrosecond(1)"
         },
         {
             "print endofyear(datetime(2017-01-01 10:10:17), -1) ",
-            "SELECT (toDateTime(toStartOfDay(parseDateTime64BestEffortOrNull('2017-01-01 10:10:17', 9, 'UTC')), 9, 'UTC') + toIntervalYear(-1 + 1)) - toIntervalMicrosecond(1)"
+            "SELECT (toDateTime(toStartOfDay(toDateTime64('2017-01-01 10:10:17', 9, 'UTC')), 9, 'UTC') + toIntervalYear(-1 + 1)) - toIntervalMicrosecond(1)"
         },
         {
            "print endofyear(datetime(2017-01-01 10:10:17), 1)" ,
-           "SELECT (toDateTime(toStartOfDay(parseDateTime64BestEffortOrNull('2017-01-01 10:10:17', 9, 'UTC')), 9, 'UTC') + toIntervalYear(1 + 1)) - toIntervalMicrosecond(1)"
+           "SELECT (toDateTime(toStartOfDay(toDateTime64('2017-01-01 10:10:17', 9, 'UTC')), 9, 'UTC') + toIntervalYear(1 + 1)) - toIntervalMicrosecond(1)"
         },
         {
             "print make_datetime(2017,10,01)",
@@ -129,55 +131,11 @@ INSTANTIATE_TEST_SUITE_P(ParserKQLQuery, ParserTest,
         },
         {
             "print datetime_diff('year',datetime(2017-01-01),datetime(2000-12-31))",
-            "SELECT dateDiff('year', parseDateTime64BestEffortOrNull('2017-01-01', 9, 'UTC'), parseDateTime64BestEffortOrNull('2000-12-31', 9, 'UTC')) * -1"
+            "SELECT dateDiff('year', toDateTime64('2017-01-01', 9, 'UTC'), toDateTime64('2000-12-31', 9, 'UTC')) * -1"
         },
         {
             "print datetime_diff('minute',datetime(2017-10-30 23:05:01),datetime(2017-10-30 23:00:59))",
-            "SELECT dateDiff('minute', parseDateTime64BestEffortOrNull('2017-10-30 23:05:01', 9, 'UTC'), parseDateTime64BestEffortOrNull('2017-10-30 23:00:59', 9, 'UTC')) * -1"
-        },
-        {
-            "print datetime(null)",
-            "SELECT parseDateTime64BestEffortOrNull('null', 9, 'UTC')"
-        },
-        {
-            "print datetime('2014-05-25T08:20:03.123456Z')",
-            "SELECT parseDateTime64BestEffortOrNull('2014-05-25T08:20:03.123456Z', 9, 'UTC')"
-        },
-        {
-            "print datetime(2015-12-14 18:54)",
-            "SELECT parseDateTime64BestEffortOrNull('2015-12-14 18:54', 9, 'UTC')"
-        },
-        {
-            "print make_timespan(67,12,30,59.9799)",
-            "SELECT CONCAT('67.', toString(substring(toString(toTime(parseDateTime64BestEffortOrNull('0000-00-00 12:30:59.9799', 9, 'UTC'))), 12)))"
-        },
-        {
-            "print  todatetime('2014-05-25T08:20:03.123456Z')",
-            "SELECT parseDateTime64BestEffortOrNull(toString('2014-05-25T08:20:03.123456Z'), 9, 'UTC')"
-        },
-        {
-            "print format_datetime(todatetime('2009-06-15T13:45:30.6175425'), 'yy-M-dd [H:mm:ss.fff]')",
-            "SELECT concat(substring(toString(formatDateTime(parseDateTime64BestEffortOrNull(toString('2009-06-15T13:45:30.6175425'), 9, 'UTC'), '%y-%m-%d [%H:%M:%S.]')), 1, position(toString(formatDateTime(parseDateTime64BestEffortOrNull(toString('2009-06-15T13:45:30.6175425'), 9, 'UTC'), '%y-%m-%d [%H:%M:%S.]')), '.')), substring(substring(toString(parseDateTime64BestEffortOrNull(toString('2009-06-15T13:45:30.6175425'), 9, 'UTC')), position(toString(parseDateTime64BestEffortOrNull(toString('2009-06-15T13:45:30.6175425'), 9, 'UTC')), '.') + 1), 1, 3), substring(toString(formatDateTime(parseDateTime64BestEffortOrNull(toString('2009-06-15T13:45:30.6175425'), 9, 'UTC'), '%y-%m-%d [%H:%M:%S.]')), position(toString(formatDateTime(parseDateTime64BestEffortOrNull(toString('2009-06-15T13:45:30.6175425'), 9, 'UTC'), '%y-%m-%d [%H:%M:%S.]')), '.') + 1, length(toString(formatDateTime(parseDateTime64BestEffortOrNull(toString('2009-06-15T13:45:30.6175425'), 9, 'UTC'), '%y-%m-%d [%H:%M:%S.]')))))"
-        },
-        {
-            "print format_datetime(datetime(2015-12-14 02:03:04.12345), 'y-M-d h:m:s tt')",
-            "SELECT formatDateTime(parseDateTime64BestEffortOrNull('2015-12-14 02:03:04.12345', 9, 'UTC'), '%y-%m-%e %I:%M:%S %p')"
-        },
-        {
-            "print format_timespan(time(1d), 'd-[hh:mm:ss]')",
-            "SELECT leftPad(toString(formatDateTime(toDateTime64(86400., 9, 'UTC'), '%e-[%I:%M:%S]')), length(toString(formatDateTime(toDateTime64(86400., 9, 'UTC'), '%e-[%I:%M:%S]'))) + 0, '0')"
-        },
-        {
-            "print format_timespan(time('12:30:55.123'), 'ddddd-[hh:mm:ss.ffff]')",
-            "SELECT leftPad(concat(substring(toString(formatDateTime(toDateTime64(1038655., 9, 'UTC'), '%d-[%I:%M:%S.]')), 1, position(toString(formatDateTime(toDateTime64(1038655., 9, 'UTC'), '%d-[%I:%M:%S.]')), '.')), substring(substring(toString(toDateTime64(1038655., 9, 'UTC')), position(toString(toDateTime64(1038655., 9, 'UTC')), '.') + 1), 1, 4), substring(toString(formatDateTime(toDateTime64(1038655., 9, 'UTC'), '%d-[%I:%M:%S.]')), position(toString(formatDateTime(toDateTime64(1038655., 9, 'UTC'), '%d-[%I:%M:%S.]')), '.') + 1, length(toString(formatDateTime(toDateTime64(1038655., 9, 'UTC'), '%d-[%I:%M:%S.]'))))), (length(toString(formatDateTime(toDateTime64(1038655., 9, 'UTC'), '%d-[%I:%M:%S.]'))) + 3) + 4, '0')"
-        },
-        {
-            "print datetime_part('day', datetime(2017-10-30 01:02:03.7654321))",
-            "SELECT formatDateTime(parseDateTime64BestEffortOrNull('2017-10-30 01:02:03.7654321', 9, 'UTC'), '%e')"
-        },
-        {
-            "print datetime_add('day',1,datetime(2017-10-30 01:02:03.7654321))",
-            "SELECT parseDateTime64BestEffortOrNull('2017-10-30 01:02:03.7654321', 9, 'UTC') + toIntervalDay(1)"
+            "SELECT dateDiff('minute', toDateTime64('2017-10-30 23:05:01', 9, 'UTC'), toDateTime64('2017-10-30 23:00:59', 9, 'UTC')) * -1"
         }
 
 })));   
