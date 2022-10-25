bced5201c88fff72190ecdb3e511e5234d908dfb
Yong Wang <yong.wang@ibm.com>
2022-10-25 09:24:37 -0700

    Kusto-phase3: Add top and top-hitters operator

--- src/Parsers/Kusto/ParserKQLQuery.h
+++ src/Parsers/Kusto/ParserKQLQuery.h
@@ -5,6 +5,8 @@
 
 namespace DB
 {
+using OperationsPos = std::vector<std::pair<String, IParser::Pos>>;
+
 class ParserKQLBase : public IParserBase
 {
 public:
@@ -14,13 +16,23 @@ public:
     static bool setSubQuerySource(ASTPtr & select_query, ASTPtr & source, bool dest_is_subquery, bool src_is_subquery);
     static bool parseSQLQueryByString(ParserPtr && parser, String & query, ASTPtr & select_node, int32_t max_depth);
     bool parseByString(const String expr, ASTPtr & node, const uint32_t max_depth);
+    virtual bool updatePipeLine (OperationsPos & /*operations*/, String & /*query*/) {return false;}
 };
 
 class ParserKQLQuery : public IParserBase
 {
-
+public:
+    struct KQLOperatorDataFlowState
+    {
+        String operator_name;
+        bool need_input;
+        bool gen_output;
+        bool need_reinterpret;
+        int8_t backspace_steps; // how many steps to last token of previous pipe
+    };
+    static bool getOperations(Pos & pos, Expected & expected, OperationsPos & operation_pos);
 protected:
-    static std::unique_ptr<IParserBase> getOperator(String &op_name);
+    static std::unique_ptr<ParserKQLBase> getOperator(String &op_name);
     const char * getName() const override { return "KQL query"; }
     bool parseImpl(Pos & pos, ASTPtr & node, Expected & expected) override;
 };
