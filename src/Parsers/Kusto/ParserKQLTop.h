9c3be169d0d29a27289101fd28d413ba2ee1c612
Yong Wang <yong.wang@ibm.com>
2022-10-25 09:24:37 -0700

    Kusto-phase3: Add top and top-hitters operator

---
+++ src/Parsers/Kusto/ParserKQLTop.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <Parsers/IParserBase.h>
+#include <Parsers/Kusto/ParserKQLQuery.h>
+
+namespace DB
+{
+
+class ParserKQLTop : public ParserKQLBase
+{
+protected:
+    const char * getName() const override { return "KQL top"; }
+    bool parseImpl(Pos & pos, ASTPtr & node, Expected & expected) override;
+    bool updatePipeLine (OperationsPos & operations, String & query) override;
+};
+
+}
