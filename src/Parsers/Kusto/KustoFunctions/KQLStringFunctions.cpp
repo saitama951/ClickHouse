66aad560d21b616d1815d4fe5a06e8747dd5c126
root <mallik.hassan@ibm.com>
2022-11-15 16:54:50 -0800

    fix commit issues

--- src/Parsers/Kusto/KustoFunctions/KQLStringFunctions.cpp
+++ src/Parsers/Kusto/KustoFunctions/KQLStringFunctions.cpp
@@ -65,13 +65,7 @@ bool Base64DecodeToArray::convertImpl(String & out, IParser::Pos & pos)
 
     ++pos;
     const String str = getConvertedArgument(fn_name, pos);
-<<<<<<< HEAD
-
-    out = std::format("arrayMap(x -> (reinterpretAsUInt8(x)), splitByRegexp ('',base64Decode({})))", str);
-
-=======
     out = std::format("IF((length({}) % 4) != 0, [NULL], IF(length(tryBase64Decode({})) = 0, [NULL], IF(countMatches(substring({}, 1, length({}) - 2), '=') > 0, [NULL], arrayMap(x -> reinterpretAsUInt8(x), splitByRegexp('', base64Decode(assumeNotNull(IF(length(tryBase64Decode({})) = 0, '', {}))))))))", str, str, str, str, str, str);
->>>>>>> improved functionality to work with columns
     return true;
 }
 
